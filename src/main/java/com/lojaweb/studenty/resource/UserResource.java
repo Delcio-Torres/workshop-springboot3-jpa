1da15cba015dee0f8fd7a0187e7d771a2c71775a
delcio-torres <delcio.at@gmail.com>
2022-11-25 13:20:40 -0300

    User entity and resource

---
+++ src/main/java/com/lojaweb/studenty/resource/UserResource.java
@@ -0,0 +1,38 @@
+package com.lojaweb.studenty.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lojaweb.studenty.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	
+	@RequestMapping
+	public ResponseEntity<User> findAllEntity(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
