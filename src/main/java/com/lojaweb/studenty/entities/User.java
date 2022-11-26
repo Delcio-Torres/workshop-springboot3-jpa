4e77144db5e2fbff8feefb569ca96aba85ed782f
delcio-torres <delcio.at@gmail.com>
2022-11-25 21:41:27 -0300

    Order, istant, ISO 8601

--- src/main/java/com/lojaweb/studenty/entities/User.java
+++ src/main/java/com/lojaweb/studenty/entities/User.java
@@ -1,16 +1,21 @@
 package com.lojaweb.studenty.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_users")
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
@@ -21,6 +26,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> order = new ArrayList<>();
 	
 	public User() {
 	}
@@ -72,7 +81,11 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
+	
+	public List<Order> getOrder() {
+		return order;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
---
+++ src/main/java/com/lojaweb/studenty/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.lojaweb.studenty.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lojaweb.studenty.entities.Order;
+import com.lojaweb.studenty.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
