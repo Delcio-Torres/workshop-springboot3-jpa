d28daf14f06623de8b9c27fe0486abfc559e9586
delcio-torres <delcio.at@gmail.com>
2022-11-26 22:54:33 -0300

    Exception handling - update

--- src/main/java/com/lojaweb/studenty/services/UserService.java
+++ src/main/java/com/lojaweb/studenty/services/UserService.java
@@ -13,6 +13,8 @@ import com.lojaweb.studenty.repositories.UserRepository;
 import com.lojaweb.studenty.services.exceptions.DatabaseException;
 import com.lojaweb.studenty.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -33,23 +35,28 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		try 
-		{
+		try {
 			repository.deleteById(id);
-		} 
-		catch (EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		} 
-		catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
 
 	public User update(Long id, User obj) {
+		try
+		{
 		User entity = repository.getReferenceById(id);
 		updateDate(entity, obj);
 		return repository.save(entity);
-	}
+		}
+		catch(EntityNotFoundException e)
+		{	
+			e.printStackTrace();
+			throw new ResourceNotFoundException(id);
+		}
+		}
 
 	private void updateDate(User entity, User obj) {
 		entity.setName(obj.getName());
